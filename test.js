aa6460dcf2faed80e2373f29032e011ca839639e
FreeWillaert <frederik.willaert@miaaguard.com>
2017-07-01 21:22:38 +0200

    Simple tests.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var handler = require("./src/index");
-var event = {
+const handler = require("./src/index");
+const event1 = {
     "headers": {
         "Host": "localhost:3000"
     },
@@ -16,8 +16,36 @@ var event = {
     "httpMethod": "GET",
     "queryStringParameters": null,
     "body": null,
+    "isOffline": true
 };
-var context = {};
+
+
+const event2 = {
+    "headers": {
+        "Host": "denhost"
+    },
+    "path": "/auth",
+    "pathParameters": {
+        "proxy": "auth"
+    },
+    "requestContext": {
+        "stage": "dev"
+    },
+    "resource": "/{proxy*}",
+    "httpMethod": "GET",
+    "queryStringParameters": {
+        "client_id": "foo",
+        "response_type": "id_token token",
+        "scope": "openid email",
+        "prompt": "login",
+        "nonce": "tarara7"
+    },
+    "body": null,
+    "isOffline": true
+};
+
+const event = event2;
+const context = {};
 handler.handler(event, context, function (error, result) {
     if (error) {
         console.error(error);
